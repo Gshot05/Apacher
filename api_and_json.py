1b71cff7a6b9b90c6bfcce7f4d2e1eaafd41410e
Gshot05 <114616202+Gshot05@users.noreply.github.com>
2023-06-23 23:07:49 +0300

    Update api_and_json.py

--- api_and_json.py
+++ api_and_json.py
@@ -23,19 +23,26 @@ def get_all_ip_addresses():
 def open_date_range_form():
     date_range_form = tk.Toplevel(root)
     date_range_form.title("Выбор дат")
+    date_range_form.configure(bg="black")
 
-    start_date_label = tk.Label(date_range_form, text="Начальная дата(ГГГГ-ММ-ДД):")
+    start_date_label = tk.Label(date_range_form, text="Начальная дата (ГГГГ-ММ-ДД):", fg="white", bg="black")
     start_date_label.pack()
     start_date_entry = tk.Entry(date_range_form)
     start_date_entry.pack()
 
-    end_date_label = tk.Label(date_range_form, text="Конечная дата (необязательно):")
+    end_date_label = tk.Label(date_range_form, text="Конечная дата (необязательно):", fg="white", bg="black")
     end_date_label.pack()
     end_date_entry = tk.Entry(date_range_form)
     end_date_entry.pack()
 
-    save_button = tk.Button(date_range_form, text="Сохранить", command=lambda: get_dates_in_range(date_range_form, start_date_entry.get(), end_date_entry.get()))
+    save_button = tk.Button(date_range_form, text="Сохранить", command=lambda: get_dates_in_range(date_range_form, start_date_entry.get(), end_date_entry.get()), bg="black", fg="white")
     save_button.pack()
+
+    # Изменение фона элементов формы
+    date_range_form.configure(bg="black")
+    start_date_entry.configure(bg="white", fg="black")
+    end_date_entry.configure(bg="white", fg="black")
+    save_button.configure(bg="black", fg="white")
 
 def get_dates_in_range(date_range_form, start_date, end_date):
     date_range_form.destroy()
@@ -69,18 +76,19 @@ def exit_program():
 # Создание главного окна
 root = tk.Tk()
 root.title("Управление данными")
+root.configure(bg="black")
 
 # Создание виджетов
-label = tk.Label(root, text="Выберите действие:")
+label = tk.Label(root, text="Выберите действие:", fg="white", bg="black")
 label.pack()
 
-button1 = tk.Button(root, text="Получить все IP-адреса и сохранить в файл", command=get_all_ip_addresses)
+button1 = tk.Button(root, text="Получить все IP-адреса и сохранить в файл", command=get_all_ip_addresses, bg="black", fg="white")
 button1.pack()
 
-button2 = tk.Button(root, text="Получить даты в заданном временном промежутке и сохранить в файл", command=open_date_range_form)
+button2 = tk.Button(root, text="Получить даты в заданном временном промежутке и сохранить в файл", command=open_date_range_form, bg="black", fg="white")
 button2.pack()
 
-button3 = tk.Button(root, text="Выход", command=exit_program)
+button3 = tk.Button(root, text="Выход", command=exit_program, bg="black", fg="white")
 button3.pack()
 
 # Запуск основного цикла обработки событий
