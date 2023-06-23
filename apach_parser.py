f212c11400a2d47c7dac44941b0e4de93f03be33
Gshot05 <114616202+Gshot05@users.noreply.github.com>
2023-06-23 10:04:56 +0300

    Update apach_parser.py

--- apach_parser.py
+++ apach_parser.py
@@ -1,30 +1,34 @@
-from ua_parser import user_agent_parser
+from datetime import datetime
 
 def parse_logs(log_path, conn):
     # Открываем файл с логами
     with open(log_path, 'r') as f:
         for line in f:
             # Разбираем строку лога
-            log_parts = line.split()
+            log_parts = line.split('"')
 
             # Проверяем, что количество частей лога достаточно
-            if len(log_parts) < 10:
+            if len(log_parts) < 3:
                 # Лог не содержит всех необходимых данных, пропускаем его
                 continue
 
             try:
                 # Извлекаем нужные данные
-                ip_address = log_parts[0]
-                date = log_parts[3].strip('[]')
-                request_method = log_parts[5].strip('"')
-                url = log_parts[6]
-                http_version = log_parts[7].split('/')[1].strip()# Удаляем лишние пробелы
-                status_code = int(log_parts[8])
-                response_size = int(log_parts[9])
+                log_info = log_parts[0].split()
+                ip_address = log_info[0]
+                date_str = log_info[3].lstrip('[')
+                date = datetime.strptime(date_str, '%d/%b/%Y:%H:%M:%S')
+                request_parts = log_parts[1].split()
+                request_method = request_parts[0]
+                url = request_parts[1]
+                http_version = request_parts[2]
+                status_code = int(log_parts[2].split()[0])
+                response_size = int(log_parts[2].split()[1])
 
                 # Сохраняем данные в БД
                 cur = conn.cursor()
-                cur.execute("INSERT INTO access_logs (ip_address, date, request_method, url, http_version, status_code, response_size) VALUES (%s, %s, %s, %s, %s, %s, %s)", (ip_address, date, request_method, url, http_version, status_code, response_size))
+                cur.execute("INSERT INTO access_logs (ip_address, date, request_method, url, http_version, status_code, response_size) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                            (ip_address, date, request_method, url, http_version, status_code, response_size))
                 conn.commit()
             except IndexError:
                 # Ошибка индекса - формат записи лога неверен
@@ -38,3 +42,4 @@ def parse_logs(log_path, conn):
                 # Обработка других исключений
                 print("Ошибка при обработке строки лога:", str(e))
                 continue
+
